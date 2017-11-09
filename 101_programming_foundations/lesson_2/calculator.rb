14bb6decee487e9a9f8f4d608a5b441ab8943134
kmeyerhofer <k@kcmr.io>
2017-11-08 19:09:09 -0800

    Adds extra functionality to calculator program.

--- 101_programming_foundations/lesson_2/calculator.rb
+++ 101_programming_foundations/lesson_2/calculator.rb
@@ -1,32 +1,37 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+#puts MESSAGES.inspect
+
 def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(num)
-  num.to_i != 0
+def number?(num)
+  /^\d+$/.match(num) || /\d{1,}\.\d{1,}/.match(num)
 end
 
 def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
+  case_statement = case op
+                    when '1'
+                      'Adding'
+                    when '2'
+                      'Subtracting'
+                    when '3'
+                      'Multiplying'
+                    when '4'
+                      'Dividing'
+                    end
+  case_statement
 end
 
-prompt("Welcome to Calculator! Enter your name:")
+prompt(MESSAGES['welcome'])#{}"Welcome to Calculator! Enter your name:")
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt("Make sure to use a valid name.")
+    prompt(MESSAGES['valid_name'])#{}"Make sure to use a valid name.")
   else
     break
   end
@@ -37,23 +42,23 @@ prompt("Hi #{name}!")
 loop do # main loop
   number1 = ''
   loop do
-    prompt("What's the first number?")
+    prompt(MESSAGES['first_number'])#{}"What's the first number?")
     number1 = gets.chomp
-    if valid_number?(number1)
+    if number?(number1)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number.")
+      prompt(MESSAGES['invalid_number'])#{}"Hmm... that doesn't look like a valid number.")
     end
   end
 
   number2 = ''
   loop do
-    prompt("What's the second number?")
+    prompt(MESSAGES['second_number'])#{}"What's the second number?")
     number2 = gets.chomp
-    if valid_number?(number2)
+    if number?(number2)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number.")
+      prompt(MESSAGES['invalid_number'])#{}"Hmm... that doesn't look like a valid number.")
     end
   end
 
@@ -74,7 +79,7 @@ loop do # main loop
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("Must choose 1, 2, 3, or 4.")
+      prompt(MESSAGES['invalid_operator'])#{}"Must choose 1, 2, 3, or 4.")
     end
   end
 
@@ -82,19 +87,19 @@ loop do # main loop
 
   result = case operator
            when '1'
-             number1.to_i + number2.to_i
+             number1.to_f + number2.to_f
            when '2'
-             number1.to_i - number2.to_i
+             number1.to_f - number2.to_f
            when '3'
-             number1.to_i * number2.to_i
+             number1.to_f * number2.to_f
            when '4'
              number1.to_f / number2.to_f
            end
 
   prompt("The Result is #{result}.")
-  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  prompt(MESSAGES['repeat_calculator'])#{}"Do you want to perform another calculation? (Y to calculate again)")
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for using the calculator. Good bye!")
+prompt(MESSAGES['goodbye'])#{}"Thank you for using the calculator. Good bye!")
