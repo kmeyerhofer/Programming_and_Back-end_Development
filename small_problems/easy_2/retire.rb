3ebd0fadbd27291e787fd3f5215de4e28e3b4b62
kmeyerhofer <k@kcmr.io>
2017-11-14 11:13:46 -0800

    Refactors with Time class.

--- small_problems/easy_2/retire.rb
+++ small_problems/easy_2/retire.rb
@@ -1,4 +1,4 @@
-current_year = 2017
+current_year = Time.now.year
 
 puts 'What is your age?'
 age = gets.chomp.to_i
@@ -6,7 +6,7 @@ puts 'At what age would you like to retire?'
 retirement_age = gets.chomp.to_i
 
 work_years = retirement_age - age
-retirement_year = current_year + age
+retirement_year = current_year + work_years
 
 puts "It's #{current_year}. You will retire in #{retirement_year}."
 puts "You only have #{work_years} years of work to go!"
