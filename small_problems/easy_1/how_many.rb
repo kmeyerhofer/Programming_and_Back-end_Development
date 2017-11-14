dec6141a37ad403a8c4dee3d845f16b464fe00b2
kmeyerhofer <k@kcmr.io>
2017-11-13 23:00:11 -0800

    Adds printing functionality.

--- small_problems/easy_1/how_many.rb
+++ small_problems/easy_1/how_many.rb
@@ -5,7 +5,10 @@ def count_occurrences(vehicle_list)
   vehicle_list.each do |veh|
     vehicle_hash.store("#{veh}", (vehicle_list.count(veh)))
   end
-  puts vehicle_hash
+
+  vehicle_hash.each do |veh, count|
+    puts "#{veh} => #{count}"
+  end
 end
 
 
