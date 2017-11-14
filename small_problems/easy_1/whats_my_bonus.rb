670ac050b69d976ad118fe633a458563af9f5e7e
kmeyerhofer <k@kcmr.io>
2017-11-14 09:58:34 -0800

    Displays bonus of a given salary.

---
+++ small_problems/easy_1/whats_my_bonus.rb
@@ -0,0 +1,8 @@
+def calculate_bonus(num, t_f)
+  t_f ? (num / 2) : 0
+end
+
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
