2ed8dddd6b4347ce405c6fab6db88f151bb12fbd
kmeyerhofer <k@kcmr.io>
2017-11-19 13:31:40 -0800

    Refactors solution.

--- small_problems/easy_3/search101.rb
+++ small_problems/easy_3/search101.rb
@@ -2,28 +2,22 @@ def prompt(message)
   puts "==> #{message}"
 end
 
-def get_num(num)
+def return_num(num)
   prompt("Enter the #{num} number:")
   gets.chomp.to_i
 end
 
-loop do
-  one = get_num("1st")
-  two = get_num("2nd")
-  three = get_num("3rd")
-  four = get_num("4th")
-  five = get_num("5th")
-  six = get_num("last")
+one = return_num("1st")
+two = return_num("2nd")
+three = return_num("3rd")
+four = return_num("4th")
+five = return_num("5th")
+six = return_num("last")
 
-  arr = [one, two, three, four, five]
-  arr2 = arr.select do |num|
-    num - six >= 0
-  end
+numbers = [one, two, three, four, five]
 
-  if arr2.count > 0
-    prompt("The number #{six} appears in #{arr2}.")
-  else
-    prompt("The number #{six} does not appear in #{arr}.")
-  end
-  break
+if numbers.include?(six)
+  prompt("The number #{six} appears in #{numbers}.")
+else
+  prompt("The number #{six} does not appear in #{numbers}.")
 end
