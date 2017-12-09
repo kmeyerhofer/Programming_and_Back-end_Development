06e62bd588504963e4477f0ba5b5d4f0806aa299
kmeyerhofer <k@kcmr.io>
2017-12-09 11:39:36 -0800

    Refactors solution.

--- 101_programming_foundations/lesson_5/practice_problem_8.rb
+++ 101_programming_foundations/lesson_5/practice_problem_8.rb
@@ -1,8 +1,8 @@
 require 'pry'
 hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
 
-hsh.each do |hashes|
-  hashes[1].each do |words|
+hsh.each do |_, values|
+  values.each do |words|
     words.scan(/[aeiou]/) {|x| puts x}
   end
 end
