5b7dd76dc5e9c851b2c175d7c0b9c7da7049df49
kmeyerhofer <k@kcmr.io>
2017-12-11 10:06:30 -0800

    Refactors solution.

--- 101_programming_foundations/lesson_5/practice_problem_15.rb
+++ 101_programming_foundations/lesson_5/practice_problem_15.rb
@@ -1,7 +1,7 @@
 arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
 
-array = arr.map do |hash|
-  hash.select do |_, value|
+array = arr.select do |hash|
+  hash.all? do |_, value|
     value.all? {|num| num.even?}
   end
 end
