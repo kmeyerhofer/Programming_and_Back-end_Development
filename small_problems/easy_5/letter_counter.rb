21bb9e61ac67dc63c2a5994b4965245cabfa65ae
kmeyerhofer <k@kcmr.io>
2018-01-07 12:01:19 -0800

    Solution to Part 2.

--- small_problems/easy_5/letter_counter.rb
+++ small_problems/easy_5/letter_counter.rb
@@ -1,6 +1,6 @@
 def word_sizes(string)
   hash = {}
-  string.split(' ').each do |word|
+  string.split.map {|sentence| sentence.gsub(/[^a-zA-Z]/, '')}.each do |word|
     hash.include?(word.length) ? hash[word.length] = hash[word.length] + 1 :
     hash[word.length] = 1
   end
@@ -9,7 +9,7 @@ end
 
 
 
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 p word_sizes('') == {}
