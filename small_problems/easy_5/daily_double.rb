d36956200f98c1f51604401168e4a3a85b2e24cb
kmeyerhofer <k@kcmr.io>
2018-01-07 16:26:24 -0800

    Refactored Solution.

--- small_problems/easy_5/daily_double.rb
+++ small_problems/easy_5/daily_double.rb
@@ -1,22 +1,11 @@
-def crunch(sentence)
-  new_sentence = sentence.split.map do |word|
-    counter = 0
-    new_word = word.split('')
-    loop do
-    if new_word[0] == new_word[1]
-      new_word.delete_at(0)
-      counter += 1
-    elsif new_word[counter] == new_word[counter + 1]
-      new_word.delete_at(counter)
-      counter += 1
-    else
-      counter += 1
-    end
-    break if counter == word.length
-    end
-    new_word.join('')
+def crunch(text)
+  index = 0
+  crunch_text = ''
+  while index <= text.length - 1
+    crunch_text << text[index] unless text[index] == text[index + 1]
+    index += 1
   end
-  new_sentence.join(' ')
+  crunch_text
 end
 
 p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
