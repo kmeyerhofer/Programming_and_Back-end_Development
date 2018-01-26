40f2eb34aac8292fa128cffde00d4b38efb9be25
kmeyerhofer <k@kcmr.io>
2018-01-26 14:30:21 -0800

    Solutions.

---
+++ exercises/object_oriented_programming/easy_1/refactoring_vehicles.rb
@@ -0,0 +1,43 @@
+class Vehicle
+  attr_reader :make, :model
+  def initialize(make, model)
+    @make = make
+    @model = model
+  end
+
+  def to_s
+    "#{make} #{model}"
+  end
+end
+
+class Car < Vehicle
+  def wheels
+    4
+  end
+end
+
+class Motorcycle < Vehicle
+  def wheels
+    2
+  end
+end
+
+class Truck < Vehicle
+  attr_reader :payload
+  def initialize(make, model, payload)
+    super(make, model)
+    @payload = payload
+  end
+
+  def wheels
+    6
+  end
+end
+
+car = Car.new('Subaru', 'Impreza')
+motorcycle = Motorcycle.new('Honda', 'Touring')
+truck = Truck.new('Chevy', '3500', 25_000)
+
+puts car
+puts motorcycle
+puts truck
