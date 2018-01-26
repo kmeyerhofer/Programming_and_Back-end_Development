8df9886efd06e97f2caf65366cd6d07cd972bb43
kmeyerhofer <k@kcmr.io>
2018-01-26 13:45:54 -0800

    Solutions to Exercises.

---
+++ exercises/object_oriented_programming/accessor_methods/unexpected_change.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(string)
+    @first = string.split[0]
+    @last = string.split[1]
+  end
+
+  def name
+    "#{@first} #{@last}"
+  end
+end
+
+person1 = Person.new
+person1.name = 'John Doe'
+puts person1.name
