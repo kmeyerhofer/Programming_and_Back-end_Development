040a5061aef7d286daed270f75e9013b615c1775
kmeyerhofer <k@kcmr.io>
2018-03-23 16:01:28 -0700

    Displays each fine within '/data/'

--- 170_web_development/lesson_9/cms-project/content_management.rb
+++ 170_web_development/lesson_9/cms-project/content_management.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+helpers do
+  def only_files_with_names(&block)
+    files = Dir.new('data/')
+    files.each { |file| yield file unless file == '.' || file == '..' }
+  end
+end
+
 get '/' do
-  "Getting started."
+  erb :docs
 end
