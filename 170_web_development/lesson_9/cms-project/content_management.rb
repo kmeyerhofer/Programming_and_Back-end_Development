cb62b7f4a6c8f23fcf61cf4fbd144c0730898277
kmeyerhofer <k@kcmr.io>
2018-03-23 16:31:23 -0700

    Displays text files.

--- 170_web_development/lesson_9/cms-project/content_management.rb
+++ 170_web_development/lesson_9/cms-project/content_management.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+root = File.expand_path('..', __FILE__)
+
 helpers do
   def only_files_with_names(&block)
     files = Dir.new('data/')
@@ -11,4 +13,10 @@ end
 
 get '/' do
   erb :docs
+end
+
+get '/:file_name' do
+  filename = root + "/data/" + params[:file_name]
+  headers["Content-Type"] = "text/plain"
+  File.read(filename)
 end
